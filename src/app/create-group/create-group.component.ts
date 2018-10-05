b8448c85c9f25fff60acef54e4bac08763141a72
mmazzaferro <38482559+mmazzaferro@users.noreply.github.com>
2018-10-05 10:06:39 +1000

    Groups can be created and added to database

--- src/app/create-group/create-group.component.ts
+++ src/app/create-group/create-group.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-create-group',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-group.component.css']
 })
 export class CreateGroupComponent implements OnInit {
-
-  constructor() { }
+  name:string
+  users
+  constructor(private router: Router, private http: HttpClient, private formsModule: FormsModule) { }
 
   ngOnInit() {
+  }
+  createGroup(event){
+    this.http.get("addGroup?name="+this.name+"&users="+this.users).subscribe(res => {
+      alert("Group Created!")
+      this.router.navigateByUrl('/groups')
+    })
   }
 
 }
